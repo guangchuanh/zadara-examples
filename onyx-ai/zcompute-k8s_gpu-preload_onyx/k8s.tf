22f00f5aadba75948638fe3c83359e3ff8bd3811
guangchuanh <154308461+guangchuanh@users.noreply.github.com>
2025-05-21 18:52:12 +0900

    Update k8s.tf

--- onyx-ai/zcompute-k8s_gpu-preload_onyx/k8s.tf
+++ onyx-ai/zcompute-k8s_gpu-preload_onyx/k8s.tf
@@ -15,8 +15,8 @@ variable "k8s_version" {
 
 module "k8s" {
   #source = "../../../terraform-zcompute-k8s"
-  source = "github.com/zadarastorage/terraform-zcompute-k8s?ref=main"
-  # source = "github.com/guangchuanh/terraform-zcompute-k8s?ref=main"
+  # source = "github.com/zadarastorage/terraform-zcompute-k8s?ref=main"
+  source = "github.com/guangchuanh/terraform-zcompute-k8s?ref=main"
   # It's recommended to change `main` to a specific release version to prevent unexpected changes
 
   vpc_id  = module.vpc.vpc_id
